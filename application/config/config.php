f16b8bd11a819572c4a22e3db172a14f57fe652e
Amirah AB Zubaidi <amirah.zubaidi1308@gmail.com>
2025-08-11 23:17:40 +0700

    mengubah config base url yang semula yang sudah dibuat

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $config['base_url'] = 'http://localhost/udzubaidi/';
 
-$config['base_url'] = 'https://udzubaidi.my.id/udzubaidi-main/';
+$config['base_url'] = 'https://udzubaidi.my.id/';
 
 /*
 |--------------------------------------------------------------------------
